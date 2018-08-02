18015675f01403a15dfceb0ca3b9f499f9f422f5
Vitor Baggi de Almeida <vitor.baggi@aluno.ufabc.edu.br>
2018-08-02 09:57:19 -0300

    diretiva para trocar a classe ao chegar no final do scroll

--- src/app/diretivas/scroll-shadow.directive.ts
+++ src/app/diretivas/scroll-shadow.directive.ts
@@ -1,22 +1,29 @@
 import { Directive, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
-  selector: '[appScrollShadow]'
+  selector: '[scroll-shadow]'
 })
 export class ScrollShadowDirective {
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
-  ngAfterContentInit(){
-    this.scrollVerify()
+  // ngAfterContentInit(){
+  //   this.scrollVerify()
+  // }
+
+  ngDoCheck(){
+    this.scrollVerify
   }
-  @HostBinding('style.box-shadow') shadow: string;
+  // @HostBinding('style.box-shadow') shadow: string;
+  @HostBinding('class.shadow') classShadow: boolean;
 
   @HostListener('scroll') scrollVerify(){
     if((this.elementRef.nativeElement.scrollHeight-this.elementRef.nativeElement.scrollTop-this.elementRef.nativeElement.clientHeight) == 0){
-      this.shadow = "none";
+      // this.shadow = "none";
+      this.classShadow = false;
     }else{
-      this.shadow = "inset -5px -5px 10px -4px";
+      // this.shadow = "inset -5px -5px 10px -4px";
+      this.classShadow = true;
     }
   }
 
